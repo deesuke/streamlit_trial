01e002a2b87ec65df7e0ed3b3bbcc26c16c48f34
deesuke <shibata.daisuke@gmail.com>
2023-03-13 22:38:47 +0900

    1st commit

---
+++ streamTrial.py
@@ -0,0 +1,14 @@
+
+import streamlit as st
+import time
+
+# プログレスバー
+latest_iteration = st.empty()
+latest_iteration = st.text(f'Iteration {1}')
+time.sleep(1)
+bar = st.progress(0)
+for i in range(100):
+    latest_iteration.text(f'Iteration {i+1}')
+    bar.progress(i + 1)
+    time.sleep(0.1)
+
